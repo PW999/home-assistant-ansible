3874af19266407a02c27ef8ff0679fd9690eee9d
Phillip <p_w999@ymail.com>
2022-01-23 18:09:57 +0100

    refactor: small code improvements
    Detected by: shellcheck

--- roles/test.sh
+++ roles/test.sh
@@ -5,45 +5,44 @@ DISTRO=$1
 # Second parameter can be used to test only a single role
 SCOPE=$2
 
-source ./setenv.sh $DISTRO
+source ./setenv.sh "$DISTRO"
 
 echo "***********************************************"
 echo "Starting with $MOLECULE_NAME: $MOLECULE_DISTRO:$MOLECULE_DISTRO_VERSION"
 
 for role in $(ls);
 do
-	if [ -f $role ];
+	if [ -f "$role" ];
 	then
 		continue
 	fi
 
-	if [ $role == 'resources' ];
+	if [ "$role" == 'resources' ];
 	then
 		continue
 	fi
-	
+
 	if [ "$SCOPE" != '' ];
 	then
-		if [ $role != $SCOPE ];
+		if [ "$role" != "$SCOPE" ];
 		then
 			continue
 		fi
 	fi
 
 	echo "*** STARTING TEST FOR $role ***"
-	mkdir -p /tmp/$MOLECULE_NAME
+	mkdir -p "/tmp/$MOLECULE_NAME"
 
-	cd $role
+	cd "$role" || exit
 	file_name="Z_$role-$MOLECULE_NAME"
 	echo "Logs are written to $file_name.out"
 
 	MOLECULE_REPORT="../$file_name.html" molecule test > "../$file_name.out" 2>&1
 	if [ $? -ne 0 ]; then
 		echo "Molecule failed for $role"
-		cat ../$file_name.out
+		cat "../$file_name.out"
 		cd ..
 		exit 1
 	fi
 	cd ..
 done
-
